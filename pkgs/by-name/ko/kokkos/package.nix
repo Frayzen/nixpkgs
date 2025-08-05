c8fa50df7aee9733840c82a1f062a51a4b019793
Frayzen <florian.laine@epita.fr>
2025-08-05 03:53:05 +0200

    wip: working on openmp + hip

--- pkgs/by-name/ko/kokkos/package.nix
+++ pkgs/by-name/ko/kokkos/package.nix
@@ -24,18 +24,7 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-gpnaxQ3X+bqKiP9203I1DELDGXocRwMPN9nHFk5r6pM=";
   };
 
-  propagatedBuildInputs =
-    [ ]
-    ++ lib.optionals (hip-backend || openmp-backend) [ rocmPackages.clang ]
-    ++ lib.optionals hip-backend [
-      rocmPackages.rocthrust
-      rocmPackages.rocprim
-      rocmPackages.rocmPath
-    ]
-    ++ lib.optionals openmp-backend [
-      llvmPackages.openmp.dev
-      llvmPackages.openmp
-    ];
+  buildInputs = [ ] ++ lib.optionals hip-backend [ rocmPackages.rocmPath ];
 
   nativeBuildInputs = [
     cmake
@@ -44,17 +33,11 @@ stdenv.mkDerivation (finalAttrs: {
 
   cmakeFlags = [
     (lib.cmakeBool "Kokkos_ENABLE_SERIAL" true)
-    (lib.cmakeBool "Kokkos_ENABLE_TESTS" true)
   ]
-  ++ lib.optionals openmp-backend [
-    (lib.cmakeBool "Kokkos_ENABLE_OPENMP" true)
-    (lib.cmakeFeature "OpenMP_CXX_FLAGS" "-fopenmp")
-    (lib.cmakeFeature "OpenMP_CXX_LIB_NAMES" "omp")
-    (lib.cmakeFeature "OpenMP_omp_LIBRARY" "${llvmPackages.openmp}/lib/libomp.so")
-    (lib.cmakeFeature "CMAKE_CXX_FLAGS" "-I${llvmPackages.openmp.dev}/include")
-  ]
+  ++ lib.optionals openmp-backend [ (lib.cmakeBool "Kokkos_ENABLE_OPENMP" true) ]
   ++ lib.optionals hip-backend [
     (lib.cmakeBool "Kokkos_ENABLE_HIP" true)
+    (lib.cmakeBool "Kokkos_ENABLE_ROCTHRUST" false)
     (lib.cmakeFeature "CMAKE_CXX_COMPILER" "hipcc")
   ]
   ++ lib.optionals (arch != "") [ (lib.cmakeBool "Kokkos_ARCH_${arch}" true) ];
